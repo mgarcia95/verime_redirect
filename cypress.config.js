8d0371ef46d0dca31583c835ae63a7d70c27ef07
mgarcia95 <manolo95gl@gmail.com>
2022-11-29 17:17:55 +0000

    Jenkinsfile&&package.json

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,14 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },chromeWebSecurity: false
+    },projectId: "mpg52y",
+  },chromeWebSecurity: false,
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports/mochawesome-report",
+    overwrite: false,
+    html: false,
+    json: true,
+    timestamp: "mmddyyyy_HHMMss"
+  }
 })
